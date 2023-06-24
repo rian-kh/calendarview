dc63c0f8567ec76c1aab7e80e5347de223b3752c
rian-kh <128095876+rkhandk@users.noreply.github.com>
2023-06-23 23:11:57 -0400

    Added auth code retrieval

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function test() {
-    alert("Hello world");
+
+// Variables
+let user;
+
+
+
+// Code ran on page load
+let authCode = new URLSearchParams(window.location.search).get('code');
+let accessToken;
+
+console.log(`Code: ${authCode}`)
+
+
+
+
+
+
+// Functions
+
+
+function redirect() {
+    console.log("Redirected to auth")
+    window.location.href = `https://api.instagram.com/oauth/authorize?client_id=${user}&redirect_uri=https://localhost:5173/&scope=user_profile,user_media&response_type=code`;
 }
+
