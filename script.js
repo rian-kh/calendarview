f53f7787042c782fbcef25070295c9fe154a7372
rian-kh <128095876+rkhandk@users.noreply.github.com>
2023-07-02 16:20:23 -0400

    Added access token retrieval

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 
 // Variables
 let user;
-
+let userId;
 
 
 // Code ran on page load
 let authCode = new URLSearchParams(window.location.search).get('code');
 let accessToken;
 
+onPageLoad();
+
 console.log(`Code: ${authCode}`)
+
+async function onPageLoad() {
+    console.log(`Code: ${authCode}`)
+
+    if (authCode) {
+        accessToken = await getAccessToken();
+    }
+}
 
 
 
@@ -23,3 +33,20 @@ function redirect() {
     window.location.href = `https://api.instagram.com/oauth/authorize?client_id=${user}&redirect_uri=https://localhost:5173/&scope=user_profile,user_media&response_type=code`;
 }
 
+async function getAccessToken() {
+    const form = new FormData();
+    form.append('client_id', user);
+    form.append('client_secret', userId);
+    form.append('grant_type', 'authorization_code');
+    form.append('redirect_uri', 'https://localhost:5173/');
+    form.append('code', authCode);
+
+    
+    let result = fetch('http://localhost:8080/https://api.instagram.com/oauth/access_token', {
+                 method: 'POST',
+                 body: form
+                 }).then((result) => result.json());
+
+
+    console.log(result)
+}
